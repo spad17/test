4ccc9f00de94c0003d3a3c38060765a709c2b2b2
Алексей Миргородский <mirgorodsky.alexey@gmail.com>
2024-10-12 18:28:40 +0300

    Ваше сообщение

--- hello.pas
+++ hello.pas
@@ -1,3 +1,17 @@
-﻿begin
-  Print('Hello World');
+﻿program HelloWorld;
+
+
+var language: string;
+
+begin
+  Writeln('На каком языке вы говорите? (русский, английский, испанский)');
+  Readln(language);
+  if language = 'русский' then
+    Println('Привет, мир!')
+  else if language = 'английский' then
+    Println('Hello, world!')
+  else if language = 'испанский' then
+    Println('¡Hola, mundo!')
+  else
+    Println('Язык не распознан.');
 end.
